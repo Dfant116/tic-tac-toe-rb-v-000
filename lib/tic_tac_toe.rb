0400866a1821ee06818240da6c2f036d519d7038
Dalton Fant <daltonfant16@gmail.com>
2018-01-09 21:59:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, position, char)
-  else !valid_move?(board, index)
+  else 
     puts "invalid"
     turn(board)
   end
